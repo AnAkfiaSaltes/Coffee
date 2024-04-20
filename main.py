ba312caaf27af15dd6848d0da968d46300bc6de9
Andrew <150067701+AnAkfiaSaltes@users.noreply.github.com>
2024-04-20 21:21:54 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,32 +1,16 @@
 from flask import Flask, render_template, request
-import os
-
 app = Flask(__name__)
-
-selected_item = None
-prices = {
-    "your_image1.jpg": 10,
-    "your_image2.jpg": 20,
-    "your_image3.jpg": 30,
-    "your_image4.jpg": 40,
-    "your_image5.jpg": 50,
-    "your_image6.jpg": 60
-}
-
 @app.route('/')
-@app.route('/index')
 def index():
     return render_template('index.html')
-
-@app.route('/select_item', methods=['POST'])
-def select_item():
-    global selected_item
-    item_index = int(request.form['item_index'])
-    if item_index == 7:
-        return selected_item
-    else:
-        selected_item = request.form['item_price']
-        return "Item price added to folder."
+@app.route('/add_to_cart', methods=['POST'])
+def add_to_cart():
+    # Получаем данные из запроса
+    image_url = request.form['image_url']
+    product_name = request.form['product_name']
+    # Добавляем логику для добавления товара в корзину
+    # Здесь может быть ваша логика для работы с корзиной, например, добавление товара в базу данных
+    return f'The product {product_name} with image {image_url} has been added to the cart!'
 
 
 if __name__ == '__main__':
